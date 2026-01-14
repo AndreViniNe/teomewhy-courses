0daeb3ea8125aa1cc4da34a76ee748ea3533f6b6
AndreViniNe <andrevini.neves@gmail.com>
2026-01-14 13:34:48 -0300

    feat: machine learning fruits and beers examples

---
+++ machine-learning-2025/cerveja.py
@@ -0,0 +1,41 @@
+# %%
+import pandas as pd
+
+# %%
+df = pd.read_excel('data/dados_cerveja.xlsx')
+df.head()
+
+# %%
+features = ['temperatura', 'copo', 'cor']
+target = 'classe'
+
+X = df[features]
+y = df[target]
+
+X.replace(
+    {"mud": 1, "pint": 2,
+     "sim": 1, "não": 0,
+     "clara": 0, "escura": 1},
+    inplace=True
+)
+
+X
+
+# %%
+from sklearn import tree
+
+model = tree.DecisionTreeClassifier()
+
+# %%
+model.fit(X, y)
+
+# %%
+import matplotlib.pyplot as plt
+
+tree.plot_tree(model, 
+               feature_names=features,
+               class_names=model.classes_,
+               filled=True
+            )
+
+# %%
